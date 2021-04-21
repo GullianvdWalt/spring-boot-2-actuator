07ee23c6aec8e6f2c673690964377e6038197f88
Gullian <gullianvdwalt1@gmail.com>
2021-04-21 06:54:40 +0200

    Init

---
+++ src/main/java/com/gvdw/springbootactuator/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package com.gvdw.springbootactuator.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created By Gullian Van Der Walt at 06:52 on Apr, 2021
+ */
+@RestController
+@RequestMapping("/spring-boot-actuator")
+public class IndexController {
+
+    @GetMapping
+    public String getTestEndPoint(){
+        return "Welcome to Spring boot 2 Actuator example";
+    }
+}
